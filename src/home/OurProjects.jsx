7a7ff3f430e2f185dbb128f0335c54a9b87748e4
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-05-16 16:30:58 +0100

    scroll hide

--- src/home/OurProjects.jsx
+++ src/home/OurProjects.jsx
@@ -12,10 +12,26 @@ import novels from "../assets/novels.jpg"
 import webTemplate from "../assets/web template.jpg"
 import paintings from "../assets/paintings.jpg"
 import ideas from "../assets/ideas.jpg"
+import { useScroll } from "react-use-gesture";
+import { animated, useSpring } from "react-spring";
+
 
 function OurProjects() {
 
 const [ showCard, setShowCard] = useState([false, false, false, false])
+
+
+
+const [style, set] = useSpring(() => ({
+    transform: "perspective(500px) rotateY(0deg)"
+  }));
+const bind = useScroll(event => {
+    set({
+      transform: `perspective(500px) rotateY(${
+        event.scrolling ? event.delta[0] : 0
+      }deg)`
+    });
+  });
 
 
     const myCategories = [
@@ -62,11 +78,13 @@ const [ showCard, setShowCard] = useState([false, false, false, false])
         
         
             <Row className="project-container row-cols-1 row-cols-sm-2  row-cols-md-3 row-cols-lg-5 p-5" 
-                style={{display:"flex", flexWrap:"nowrap", overflowX:"scroll", overflowY:"hidden",
-               }}> 
-                <Col style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                    <ArrowBackIos className="text-white"/>
-                <h1 style={{margin:"30px auto", color:"white",transform: "rotate(270deg)"}}>Our Projects</h1>
+                style={{display:"flex", flexWrap:"nowrap", overflowX:"scroll", overflowY:"hidden", 
+               }} {...bind()}> 
+                <Col style={{display:"flex", justifyContent:"center", alignItems:"center",position:"relative"}}>
+                    <div style={{ display:"flex", justifyContent:"center", alignItems:"center", position:"absolute", }}>
+                    <ArrowBackIos className="text-white pointer" style={{  right:0}}/>
+                    <h1 style={{margin:"30px auto", color:"white",transform: "rotate(270deg)"}}>Our Projects</h1>
+                    </div>
                 </Col>
             
 
@@ -76,6 +94,9 @@ const [ showCard, setShowCard] = useState([false, false, false, false])
                </Col>
                
         )}
+            <Col style={{display:"flex", justifyContent:"center", alignItems:"center",position:"relative"}}>
+                    <ArrowForwardIos className="text-white pointer" style={{  right:0}}/>
+            </Col>
         </Row>
 
         
