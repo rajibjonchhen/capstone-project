7696847434351be10f6f790e35ef21a1092a8e31
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-05-16 15:46:57 +0100

    messages displayed

--- src/profile/MyMessages.jsx
+++ src/profile/MyMessages.jsx
@@ -1,5 +1,6 @@
 import { Avatar, Grid } from "@material-ui/core";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,9 +9,10 @@ import { setAllChatsAction, setMyMessagesAction, setSingleProductAction } from "
 import "./myMessage.css";
 function MyMessages() {
   const [error, setError] = useState();
-  const [singleMsg, setSingleMsg] = useState({});
+  const [singleChat, setSingleChat] = useState({});
   const [isLoading, setIsLoading] = useState();
 
+  const myInfo = useSelector((state) => state.user.myInfo);
   const myMessages = useSelector((state) => state.user.myMessages);
   const allChats = useSelector((state) => state.chat.allChats);
   const navigate = useNavigate();
@@ -44,42 +46,42 @@ function MyMessages() {
     }
   };
 
-  useState(() => {
+  useEffect(() => {
     getMessages();
   }, []);
 
   return (
-    <Container className="py-3">
-    <Row container className=" mt-3 py-3">
-      <Col item xs={12} md={6} lg={4} className="msg-sender-list-box p-1">
+    <Container  className="py-3">
+    <Row  className=" mt-3 py-3">
+      <Col  xs={12} md={6} lg={4} className="msg-sender-list-box p-1">
           <h3>Messages</h3>
           <hr className="bg-light pb-0 mb-0"/>
         {allChats.map((chat, i ) => (
             <div key={i}>
-              {chat.members.map((member, i) => {
+              
                 <div
                     className="sender-list "
                     onClick={() => {
-                        setSingleMsg(chat);
+                      setSingleChat(chat);
                     }}
                     >
-                      hello
                     <span className="m-1">
-                        <Avatar src={member?.avatar}/>
+                        <Avatar src={chat?.members[1]?.avatar}/>
                     </span>
                       <span className="m-1">
-                        {member?.name.toUpperCase()}{" "}
-                        {member?.surname.toUpperCase()}
+                        {chat?.members[1]?.name.toUpperCase()}{" "}
+                        {chat?.members[1]?.surname.toUpperCase()}
                     </span>
                 </div>
-                    })}
+                  
         </div>
         ))}
       </Col>
       <Col xs={12} md={6} lg={8} item className="msg-detail-box p-1">
-        {singleMsg &&
-         (
-          <Box>
+      
+        {Object.keys(singleChat).length > 0 &&
+         
+          <div >
 
         <h3>Message Detail</h3>
           <div style={{ textAlign: "left", margin: "0 0 0 10px" }}>
@@ -87,28 +89,63 @@ function MyMessages() {
             <div className="message-element">
               {" "}
               <span className="m-1">
-                <Avatar src={singleMsg?.sender?.avatar} className="P-1"/>
+                <Avatar src={singleChat?.members[1]?.avatar} className="p-1"/>
               </span>
               <span className="m-1" style={{fontSize:"15px", fontWeight:"600"}}>
-                {singleMsg?.sender?.name.toUpperCase()} {singleMsg?.sender?.surname.toUpperCase()}
+                {singleChat?.members[1]?.name.toUpperCase()} {singleChat?.members[1]?.surname.toUpperCase()}
               </span>
               
             </div>
-            <p className="message-element-title">Text :</p>
-            <p className="message-element">{singleMsg.text}</p>
-            <p className="message-element-title">Place :</p>
-            <p className="message-element">{singleMsg.place}</p>
+          
+            <div style={{overflow:"scroll", alignSelf:"stretch", height:"70vh"}}>
+
+            
+            {singleChat?.messages?.map((message) => 
+            <div>
+              {/* <p className="message-element">Text : {message.text}</p> */}
+              <div >
+                
+                <div  className={`p-2 w-50 ${message.sender === myInfo?._id?  "ml-auto":"mr-auto"}`}>
+                    <div className="d-flex">
+                        <span className="m-1" style={{flex:"1", order:message?.sender?._id === myInfo?._id? 2:1}}>
+                          <Avatar src={message?.sender?.avatar} className="p-1"/>
+                        </span>
+                        <span>
+                          {message?.sender?.name}
+                        </span>
+
+                        <div>
+                          <p className='single-message' style={{flex:"5",}}>
+                              {message?.text}
+                          </p>
+                          <p style={{fontSize:'10px'}}>
+                        <span className="m-1">
+                            {new Date(message?.createdAt).toLocaleTimeString()}   
+                        </span>
+                        <span className="m-1">
+                            {new Date(message?.createdAt).toLocaleDateString()} 
+                        </span>
+                      </p>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            </div>
+            ) 
+          }
           </div>
-        <div onClick={() =>{dispatch(setSingleProductAction(singleMsg.product)); navigate("/detail")}}>
-          <h5>{singleMsg?.product?.title}</h5>
-          <img
-            src={singleMsg?.product?.images[0]}
-            alt={singleMsg?.product?.title}
-            style={{ width: "200px" }}
-            />
-        </div>
-            </Box>
-          )}
+          </div>
+            {singleChat?.product && <div >
+              <h5>{singleChat?.product?.title}</h5>
+              <img
+                src={singleChat?.product?.images[0]}
+                alt={singleChat?.product?.title}
+                style={{ width: "200px" }}
+                />
+            </div>}
+            </div>
+          }
+       
           </Col>
     </Row>
     </Container>
